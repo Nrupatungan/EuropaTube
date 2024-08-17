5246e5f0cfe92e8a667369564892a3b78ed7e535
Dinesh Sake <dineshsake12345@gmail.com>
2024-08-17 23:18:24 +0530

    Mod: Optimized code and added skeletons for loading effect.

--- frontend/src/Pages/Home.jsx
+++ frontend/src/Pages/Home.jsx
@@ -1,8 +1,8 @@
-import { VideoCard } from "@/components/VideoCard"
+import { Skeleton } from "@/components/ui/skeleton";
+import { VideoCard } from "@/components/VideoCard";
 import { formatDuration, timeSince, viewsFormatter } from "@/utils/helper";
 import { fetchAllVideos } from "@/utils/queryApis";
-import { useInfiniteQuery} from "@tanstack/react-query";
-import { useEffect} from "react";
+import { useInfiniteQuery } from "@tanstack/react-query";
 import { useSelector } from "react-redux";
 
 export const Home = () => {
@@ -13,40 +13,38 @@ export const Home = () => {
     console.log(`Clicked video with ID: ${videoId}`);
   };
 
-  const {
-    data,
-    isLoading,
-    error,
-    refetch,
-  } = useInfiniteQuery({
+  const { data, isLoading, error} = useInfiniteQuery({
     queryKey: ['videos', query],
-    queryFn: ({ pageParam = 1 }) => fetchAllVideos({query, pageParam}),
+    queryFn: ({ pageParam = 1 }) => fetchAllVideos({ query, pageParam }),
     getNextPageParam: (lastPage) => lastPage.nextPage,
-    select: data => data || [],
+    select: (data) => data.pages.flatMap((page) => page.data) || [], // Ensure empty array on initial load
     staleTime: 1000 * 60 * 5, // 5 minutes
-    keepPreviousData: true,
   });
 
-  useEffect(() => {
-    refetch()
-  }, [query, refetch])
-
-
-  if(isLoading){
-    return <div>Loading...</div>
-  }
-
-  if(error){
-    return <div>Error: {error.message}</div>
-  }
-
-  const allItems = data?.pages?.flatMap((page) => page.data)
+  if (error) return <div>Error: {error.message}</div>;
 
   return (
     <div
     className="overflow-y-scroll min-h-screen pb-10 px-1 md:px-4 grid place-content-start gap-4 min-[1900px]:gap-10 grid-cols-[repeat(auto-fill,minmax(210px,1fr))] sm:grid-cols-[repeat(auto-fill,minmax(250px,1fr))] md:grid-cols-[repeat(auto-fill,minmax(300px,1fr))] min-[800px]:grid-cols-[repeat(auto-fill,minmax(250px,1fr))] min-[1100px]:grid-cols-[repeat(auto-fill,minmax(300px,1fr))] min-[1900px]:grid-cols-[repeat(auto-fill,minmax(450px,1fr))]"
     >
-      {allItems.map((video, index) => (
+      {isLoading 
+      ? 
+          Array(8).fill(null).map((_, i) => (
+            <div
+            key={i}
+            className="h-full w-full max-w-[350px] rounded-xl relative cursor-wait"
+          >
+            <Skeleton className="h-[208px] w-full rounded-xl" />
+            <div className="flex w-full justify-around items-center p-2 gap-2">
+              <Skeleton className="h-10 w-10 rounded-full bg-slate-400" />
+              <div className="w-full grid gap-2">
+                <Skeleton className="h-3 bg-slate-400 rounded-xl" />
+                <Skeleton className="h-3 bg-slate-400 rounded-xl" />
+              </div>
+            </div>
+          </div>
+          ))
+      : data?.map((video, index) => (
             <VideoCard
                    key={index}
                    className='w-full md:w-full cursor-pointer'
@@ -59,7 +57,12 @@ export const Home = () => {
                    duration={formatDuration(video.duration)}
                    onClickFunction={() => onClick(video._id)}
                />
-          ))}
+
+          ))
+
+      }
+
     </div>
+
   )
-}
+};
--- frontend/src/components/Header/SearchBarDesktop.jsx
+++ frontend/src/components/Header/SearchBarDesktop.jsx
@@ -1,120 +1,70 @@
-import { useEffect, useRef, useState } from "react";
+import { useRef, useState } from "react";
 import { Input } from "../ui/input";
 import { GiCrossedSabres } from "react-icons/gi";
-import { debounce } from "@/utils/helper";
 import { CiSearch } from "react-icons/ci";
 import { GoArrowUpLeft } from "react-icons/go";
-import { useQueryClient } from "@tanstack/react-query";
+import { useQuery } from "@tanstack/react-query";
 import { useDispatch } from "react-redux";
 import { setSearchQuery } from "@/store/slices/querySlice";
+import { fetchAllVideos } from "@/utils/queryApis";
+import { debounce } from "@/utils/helper";
 
 export const SearchBarDesktop = () => {
-    const containerRef = useRef(null)
-    const searchInputRef = useRef(null)
-    const [query, setQuery] = useState('')
-    const [searchResults, setSearchResults] = useState([]);
-    const searchQuery = new RegExp(query, 'i');
-    const queryClient = useQueryClient()
-    const data = queryClient.getQueryData(['videos'])
-    const videos = data?.pages.flatMap(page => page.data) || []
-    const dispatch = useDispatch()
-    
+  const containerRef = useRef(null);
+  const searchInputRef = useRef(null);
+  const [query, setQuery] = useState('');
+  const dispatch = useDispatch();
 
-    const handleSearch = debounce((value) => {
-        // Your search logic here
-        setQuery(value);
-    }, 300);
+  const { data } = useQuery({
+    queryKey: ['searchResults', query],
+    queryFn: () => fetchAllVideos({ query, pageParam: 1 }),
+    select: (data) => data?.data || [],
+    enabled: !!query,
+    staleTime: 1000 * 60 * 5, // 5 minutes
+  });
 
-    const handleChange = (e) => {
-        const {value} = e.target;
-        handleSearch(value);
-        if(value !== ''){
-            containerRef.current.classList.remove('hidden')
-        }else{
-            containerRef.current.classList.add('hidden')
-        }
-    }
+  const handleClear = () => {
+    searchInputRef.current.value = '';
+    setQuery('');
+    containerRef.current.classList.add('hidden');
+  };
 
-    const handleClear = () => {
-        searchInputRef.current.value = ''
-        setQuery('')
-        setSearchResults([])
-        containerRef.current.classList.add('hidden')
-    }
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    containerRef.current.classList.add('hidden');
+    dispatch(setSearchQuery(query));
+  };
 
-    //Clear the results container on input blur
-    // const hideContainer = () => {
-    //     containerRef.current.classList.add('hidden')
-    // }
-
-    const handleSubmit = (e) => {
-        e.preventDefault()
-        containerRef.current.classList.add('hidden')
-        dispatch(setSearchQuery(query))
-    }
-
-    const handleSuggestion = (title) => {
-        searchInputRef.current.value = title
-        setQuery(title)
-    }
-
-    useEffect(() => {
-        const uniqueOwners = new Set();
-        const extractedResults = [];
-      
-        videos.forEach(video => {
-          const titleMatch = video.title.match(searchQuery);
-          const channelNameMatch = video.owner.fullName.match(searchQuery);
-      
-          if (titleMatch || channelNameMatch) {
-            const ownerName = video.owner.fullName;
-            if (!uniqueOwners.has(ownerName)) {
-              uniqueOwners.add(ownerName);
-              extractedResults.push({
-                zab: video.title
-              })
-              extractedResults.push({
-                zab: video.description
-              })
-              extractedResults.push({
-                zab: video.owner.fullName
-              })
-            }
-          }
-        });
-      
-        setSearchResults(extractedResults.slice(0,8));
-        console.log(searchResults);
-      // eslint-disable-next-line react-hooks/exhaustive-deps
-      }, [query]);
+  const handleSuggestion = (title) => {
+    searchInputRef.current.value = title;
+    debounce(() => {setQuery(title);}, 300)
+  };
 
   return (
-        <form className="flex-1 max-lg:hidden relative" onSubmit={handleSubmit}>
-            <Input
-                className='ring-offset-background dark:border-2 focus-visible:outline-none focus-visible:ring-0 focus-visible:ring-offset-0 border-black dark:border-slate-400 pe-10 min-[1900px]:h-[55px] min-[1900px]:text-[20px]'
-                placeholder='Search EuropaTube'
-                ref={searchInputRef}
-                onChange={handleChange}
-                // onBlur={hideContainer}
-            ></Input>
-            <GiCrossedSabres className="absolute min-[1900px]:h-[20px] min-[1900px]:w-[20px] right-3 top-3 min-[1900px]:top-4 cursor-pointer active:scale-75 transition duration-200"
-                onClick={handleClear}
-            />
-            <div className="absolute bg-black/90 dark:bg-black border-2 border-lime-400 text-white w-full h-auto max-h-[300px] min-[1900px]:max-h-[500px] overflow-y-scroll overscroll-none hidden"
-            ref={containerRef}
-            >
-                {/* Add your search results here */}
-                {searchResults?.map((result, id) => {
-                return(
-                    <div key={id} className="flex gap-4 min-[1900px]:gap-5 items-center px-3 pb-3 min-[1900px]:pb-4 space-y-3 min-[1900px]:space-y-5 hover:bg-slate-500/90 hover:font-semibold cursor-pointer">
-                        <CiSearch  className="mt-3 min-[1900px]:mt-5 h-[18px] w-[18px] min-[1900px]:h-[1.8rem] min-[1900px]:w-[1.8rem]" />
-                        <h3 className="text-sm min-[1900px]:text-xl flex-1">{result.zab}</h3>
-                        <GoArrowUpLeft className="mt-3 h-[18px] w-[18px] min-[1900px]:h-[1.6rem] min-[1900px]:w-[1.6rem]" 
-                        onClick={() => handleSuggestion(result.zab)} />
-                    </div>
-                )
-                })}
-            </div>
-        </form>
+    <form className="flex-1 max-lg:hidden relative" onSubmit={handleSubmit}>
+      <Input
+        className="ring-offset-background dark:border-2 focus-visible:outline-none focus-visible:ring-0 focus-visible:ring-offset-0 border-black dark:border-slate-400 pe-10 min-[1900px]:h-[55px] min-[1900px]:text-[20px]"
+        placeholder="Search EuropaTube"
+        ref={searchInputRef}
+        onChange={(e) => setQuery(e.target.value)}
+      />
+      <GiCrossedSabres
+        className="absolute min-[1900px]:h-[20px] min-[1900px]:w-[20px] right-3 top-3 min-[1900px]:top-4 cursor-pointer active:scale-75 transition duration-200"
+        onClick={handleClear}
+      />
+      <div
+        className={`absolute bg-black/90 dark:bg-black border-2 border-lime-400 text-white w-full h-auto max-h-[300px] min-[1900px]:max-h-[500px] overflow-y-scroll overscroll-none ${query ? '' : 'hidden'}`}
+        ref={containerRef}
+      >
+        {/* Display search results using searchResults data */}
+        {data?.map((result, id) => (
+          <div key={id} className="flex gap-4 min-[1900px]:gap-5 items-center px-3 pb-3 min-[1900px]:pb-4 space-y-3 min-[1900px]:space-y-5 hover:bg-slate-500/90 hover:font-semibold cursor-pointer">
+            <CiSearch className="mt-3 min-[1900px]:mt-5 h-[18px] w-[18px] min-[1900px]:h-[1.8rem] min-[1900px]:w-[1.8rem]" />
+              <h3 className="text-sm min-[1900px]:text-xl flex-1">{result.title}</h3>
+              <GoArrowUpLeft className="mt-3 h-[18px] w-[18px] min-[1900px]:h-[1.6rem] min-[1900px]:w-[1.6rem]" onClick={() => handleSuggestion(result.title)} />
+          </div>
+        ))}
+    </div>
+  </form>
   )
 }
